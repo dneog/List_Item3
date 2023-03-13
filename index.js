84876aa713286991f6981896ab77e279c1c6a2a1
unknown <debashisneog22@gmail.com>
2023-03-13 12:34:50 +0530

    Item Lists Code

---
+++ index.js
@@ -0,0 +1,11 @@
+const li= document.getElementsByTagName("li");
+for(var i=0; i<li.length; i++){
+li[i].style.backgroundColor="#98FB98"
+}
+
+const newItem= document.getElementsByClassName("item-group");
+newItem[0].style.listStyleType="none"
+newItem[0].style.padding="10px 20px"
+newItem[0].style.border="1px solid #cccccc"
+newItem[0].style.borderRadius="0px 0px 4px 4px "
+
